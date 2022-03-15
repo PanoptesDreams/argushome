94f35c60fd0fc7b3ec1ea1cfd452f6e647455753
Adam <55162814+PanoptesDreams@users.noreply.github.com>
2022-03-16 02:08:15 +1300

    Some good stuff
    Added authorship, help, icons, JS, source link
    
    Removed jank

---
+++ index.js
@@ -0,0 +1,13 @@
+// 
+// Argus Home
+//
+// Adam Hart | help@panoptes.live
+//
+// v0
+//
+
+window.onload = function() {
+	document.getElementById("searcher").focus();
+};
+
+Console.Log('Load Complete!')
