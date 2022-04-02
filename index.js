f5f50943a63ce1bccda9e16a3902c4b7271cef17
Adam <55162814+PanoptesDreams@users.noreply.github.com>
2022-04-03 01:55:25 +1300

    Did some things
    I'll tell you later..
    
    But for now, there's a radio!

--- index.js
+++ index.js
@@ -6,8 +6,22 @@
 // v0
 //
 
+
+let datetime0 = document.querySelector('p');
+
+
 window.onload = function() {
 	document.getElementById("searcher").focus();
+	
+	let today = new Date();
+    
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    let date = today.getDate();
+
+    let current_date = `${month}/${date}/${year}`;
+    
+	document.getElementById("datetime0").innerText = current_date;
 };
 
 Console.Log('Load Complete!')
